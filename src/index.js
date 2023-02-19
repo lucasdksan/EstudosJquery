1efbd3bb0a3758649b52d309cf398bbd6b90eb26
Lucas <lucas.silva@agencian1.com.br>
2023-02-18 22:33:24 -0300

    Parada na aula 19

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-    $(".btn").on("click", function(){
+$(document).ready(function () {
+    $(".btn").on("click", function () {
         $(".title").css("color", "red")
             .text("Lucas Gostoso");
     });
@@ -31,11 +31,11 @@ $(document).ready(function(){
 
     console.log(aElement);
 
-    $(":submit").on("click", function(){
+    $(":submit").on("click", function () {
         $(":text").css("border", "3px solid red");
     });
 
-    $(".content-btn").on("click", function(){
+    $(".content-btn").on("click", function () {
         $(".name-son").attr("data-game", "valorant melhor jogo de FPS");
         $(".name-son").removeAttr("data-dad");
         $(".name-son").addClass("dev");
@@ -44,7 +44,7 @@ $(document).ready(function(){
         alert(`VALOR: ${valueData}`);
     });
 
-    $(".content-val > button").on("click", function(){
+    $(".content-val > button").on("click", function () {
         const valueP = $(".content-val > p").html(); // pode pegar o valor, alterar o conteudo de um elemento e alterar o conteudo como se fosse um outro atributo.
         const pValue = $(".content-val > p").text()
         const clone = $(".content-val > p").clone();
@@ -54,19 +54,19 @@ $(document).ready(function(){
         alert(pValue);
     });
 
-    $(".createImage-area > button").on("click", function(){
+    $(".createImage-area > button").on("click", function () {
         const skills = ["Q - CAÇADOR DE CABEÇAS", "E - RENDEZVOUS", "C - MARCA REGISTRADA", "X - TOUR DE FORCE"];
         const image = $("<img />", {
             src: "./chamber.png",
             title: "O melhor agente do Valorant",
-            click: function(){
+            click: function () {
                 alert("EU AMO ESSA ARMA ");
             },
         });
 
         const li = [];
 
-        skills.forEach((e)=>{
+        skills.forEach((e) => {
             li.push($("<li/>").append(e));
         });
 
@@ -75,7 +75,7 @@ $(document).ready(function(){
         $(".createImage-area > div.area").append(image).append(ul);
     });
 
-    $(".agents > button").not(".clear").on("click", function(){
+    $(".agents > button").not(".clear").on("click", function () {
         console.log("1")
 
         $(".agents > ul").append($("<li/>").append("viper"));
@@ -86,7 +86,7 @@ $(document).ready(function(){
         $(".agents > ul").wrap("<fieldset></fieldset>");
     });
 
-    $(".agents  button.clear").on("click", function(){
+    $(".agents  button.clear").on("click", function () {
         $(".agents > ul").empty();
         $(".agents fieldset > ul").empty();
     });
@@ -95,15 +95,32 @@ $(document).ready(function(){
     //     $(".containerEventJQUERY > .containerResult").text("Foi apenas um click normal").css("color", "red");
     // });
 
-    $(".containerEventJQUERY > button").dblclick(function(){
+    $(".containerEventJQUERY > button").dblclick(function () {
         $(".containerEventJQUERY > .containerResult").text("Foram dois clicks").css("color", "red");
     });
 
-    $(".containerEventJQUERY > button").mousemove(function(){
+    $(".containerEventJQUERY > button").mousemove(function () {
         $(this).css("background", "blue");
     });
 
-    $(".containerEventJQUERY > button").mouseout(function(){
+    $(".containerEventJQUERY > button").mouseout(function () {
         $(this).css("background", "brown");
+    });
+
+    $(".containerEventJQUERY > button").mousemove(function (e) {
+        $(".containerEventJQUERY > .containerResult").text(`X: ${e.clientX}  Y: ${e.clientY}`);
+    });
+
+    $(".formText :text").keyup(function (e) {
+        $(".whiteBoard").text(`${$(this).val()}`);
+    }).focus(function () {
+        $(this).css("border", "2px solid red");
+    }).blur(function (param) {
+        $(this).css("background","yellow");
+    });
+
+    $(".formText").submit(function(e){
+        e.preventDefault();
+        console.log(e)
     });
 });
